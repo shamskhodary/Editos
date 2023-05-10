8c6ec50917b9de1cf2fc4684e49862af8b9bb083
shamskhodary <shams.khodary@gmail.com>
2023-05-10 23:41:59 +0300

    handled navbar styling

--- client/src/componenets/Navbar.jsx
+++ client/src/componenets/Navbar.jsx
@@ -1,27 +1,45 @@
 import React from "react";
-import { Layout, Space } from "antd";
-import { AutoComplete, Input } from "antd";
-const { Header, Content } = Layout;
+import { Avatar, AutoComplete, Input, Tooltip } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import "../styles/navbar.css";
+import Typewriter from 'typewriter-effect';
 
 const Navbar = () => {
   return (
     <>
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <h1  style={{ color: "white", display: "inline" }}>Editos</h1>
-          <AutoComplete
-            popupClassName="certain-category-search-dropdown"
-            dropdownMatchSelectWidth={500}
-            style={{
-              width: 250,
+      <header className="header">
+        <div className="logo">
+          <Typewriter
+          style={{color: "white"}}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Editor")
+                .pauseFor(500)
+                .deleteChars(1)
+                .callFunction(() => {
+                  console.log("All strings were deleted");
+                })
+                .typeString("s")
+                .start();
             }}
-          >
-            <Input.Search size="large" placeholder="input here" />
-          </AutoComplete>
-        </Header>
-        <Content style={{ padding: "0 50px" }}></Content>
-      </Layout>
+          />
+        </div>
+        <AutoComplete
+          popupClassName="certain-category-search-dropdown"
+          className="auto"
+          dropdownMatchSelectWidth={500}
+          style={{
+            width: "40%",
+          }}
+        >
+          <Input.Search size="large" placeholder="input here" />
+        </AutoComplete>
+        <div className="avatar">
+        <Tooltip title="user" style={{backgroundColor: "red"}}>
+        <Avatar icon={<UserOutlined />} />
+        </Tooltip>
+        </div>
+      </header>
     </>
   );
 };
