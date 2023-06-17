27d32d8e849e966f37465a11645a03f896c37c29
shamskhodary <shams.khodary@gmail.com>
2023-06-18 00:02:13 +0300

    handled delete documents

--- client/src/componenets/Documents.jsx
+++ client/src/componenets/Documents.jsx
@@ -3,43 +3,60 @@ import "../styles/documents.css";
 import {useNavigate} from 'react-router-dom'
 import { Col, Dropdown, Row, Spin, Tooltip } from "antd";
 import { MoreOutlined, DeleteFilled, EditFilled } from "@ant-design/icons";
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const items = [
-  {
-    label: (
-      <>
-        <EditFilled /> Rename
-      </>
-    ),
-    key: "0",
-  },
-  {
-    label: (
-      <>
-        <DeleteFilled /> Remove
-      </>
-    ),
-    key: "1",
-  },
-];
 
-const Documents = ({data}) => {
+
+const Documents = ({data, isLoading, setDeleted}) => {
   const navigate = useNavigate();
-
 
   const style = {
     margin: "1rem",
   };
 
-  if(!data) return (<Spin style={{display: 'flex', alignItems: 'center'}}/>)
+  const items = [
+    {
+      label: (
+        <>
+          <EditFilled /> Rename
+        </>
+      ),
+      key: 1,
+    },
+    {
+      label: (
+        <>
+          <DeleteFilled /> Remove
+        </>
+      ),
+      key: 2,
+    },
+  ]
+
+  const handleDelete = async(key, id) =>{
+   if(+key === 2){
+    const {data} = await axios.delete(`/document/${id}`);
+    setDeleted(true);
+    toast.success(data.message);
+   }
+  }
+  
+  const handleDoc = async(id) => {
+    navigate(`/document/${id}`);
+    await axios.put(`/document/${id}`, {id});
+  }
+
 
   return (
+    <>
+    {isLoading && <Spin/>}
     <div className="documents">
       <h2>Recent documents</h2>
       <Row className="row" gutter={16}>
         {data.map((e) => 
           <Col className="gutter-row" span={4} style={style} key={e.id} >
-          <div className="content-preview" onClick={() => navigate(`/document/${e.id}`)}>
+          <div className="content-preview" onClick={() => handleDoc(e.id)}>
             <p>{e.inner_content}</p>
           </div>
           <div className="details">
@@ -48,7 +65,10 @@ const Documents = ({data}) => {
             </Tooltip>
             <div className="more-details">
               <span>Opened {moment(e.last_opened).calendar()}</span>
-              <Dropdown menu={{ items }} trigger={["click"]}>
+              <Dropdown menu={{
+                onClick:({key}) => handleDelete(key, e.id),
+                 items: items,
+                  }} trigger={["click"]}>
                 <MoreOutlined className="dots-icon" />
               </Dropdown>
             </div>
@@ -58,6 +78,8 @@ const Documents = ({data}) => {
       
       </Row>
     </div>
+    </>
+   
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,27 +2,57 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../componenets/Navbar";
 import Documents from "../componenets/Documents";
 import axios from "axios";
+import { PlusOutlined } from "@ant-design/icons";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 
 const Home = () => {
-  const [documents, setDocuments] = useState(null);
+  const [documents, setDocuments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+  const auth= useAuth();
+  const navigate = useNavigate();
+
+
 
   useEffect(()=>{
     const docs = async() => {
+      setIsLoading(true)
       try {
         const response = await axios.get('/documents');
         setDocuments(response.data);
+        setIsLoading(false)
       } catch (error) {
         setDocuments(null);
+        setIsLoading(false);
       }    
     }
 
     docs();
-  },[]);
+  },[deleted]);
+
+  const handleAddDoc = async()=> {
+    try {
+      const {data} = await axios.post('/document', {
+        title: "Untitled Document",
+        user_id: auth.user.id
+      });
+      if(data.data){
+        navigate(`/document/${data.data.id}`)
+      }
+    } catch (error) {
+      toast.error('unable to create a document')
+    }
+
+  }
 
   return (
     <>
       <Navbar />
-      {documents ? <Documents data={documents} />: <h5>No documents found</h5>}
+      {documents.length ? <Documents data={documents} isLoading={isLoading} setDeleted={setDeleted} />: <h3 style={{padding: "2rem"}}>No documents found</h3>}
+      <PlusOutlined className="plus-icon" onClick={handleAddDoc}/>
     </>
   );
 };
